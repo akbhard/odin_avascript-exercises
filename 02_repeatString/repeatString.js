79beaa85d391e4196fe4476ed504e95e15172b8e
Akshay <149981521+akbhard@users.noreply.github.com>
2024-09-04 00:21:08 -0400

    Added in all necessary code to have all seven tests succeed. Major things to flag are looping instead of adding to a pre-existing string, which I resolved. Second point was adding in an if statement before my loop to handle negative numbers.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,21 @@
-const repeatString = function() {
-
-};
+function repeatString(string,num) {
+  
+    if (num < 0) {
+        return 'ERROR'
+    }
+    
+    let i = 0
+    
+    let result = ''
+    
+    while (i < num) {
+          result += string
+          i++;
+      }
+    
+    return result
+  }
+  
 
 // Do not edit below this line
 module.exports = repeatString;
