30c02d347543218ca0b46737850b29ee69078ef0
Akshay <149981521+akbhard@users.noreply.github.com>
2024-09-04 00:27:09 -0400

    Added in code to reverse a string. Had to google again, but simply returning a string that was split, reverse, and then joined once more does the trick. All tests passed.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,5 @@
-const reverseString = function() {
-
-};
-
+function reverseString(string) {
+    return string.split('').reverse().join('')
+}
 // Do not edit below this line
 module.exports = reverseString;
