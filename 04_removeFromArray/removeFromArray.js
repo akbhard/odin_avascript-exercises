5e53f0f29fa5faa6fd7671e9eca42d3fe8a61141
Akshay <149981521+akbhard@users.noreply.github.com>
2024-09-05 00:17:29 -0400

    Added in initial function that removed argument for array. Passed the first test. Failed the second test which had multiple values for an argument. Need to figure out how to handle that

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,8 @@
-const removeFromArray = function() {
-};
-
+function removeFromArray(array, arg) {
+  
+    array.splice(array.indexOf(arg),1)
+    return array
+     
+   }
 // Do not edit below this line
 module.exports = removeFromArray;
