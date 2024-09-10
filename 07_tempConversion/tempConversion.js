7436452772f68b24b85e58df3ebd16c71ccd0b6e
Akshay <149981521+akbhard@users.noreply.github.com>
2024-09-10 00:39:13 -0400

    Easy conversion, looked up the rounding on stack overflow

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,15 @@
-const convertToCelsius = function() {
-};
+function convertToCelsius(number){
+  
+  let celsius = (number - 32) * (5/9)
+  return Math.round(celsius * 10)/10
+}
+// } (32°F − 32) × 5/9 = 0°C
 
-const convertToFahrenheit = function() {
-};
-
+function convertToFahrenheit(number){
+  
+  let fahrenheit = (number * (9/5)) + 32
+  return Math.round(fahrenheit * 10)/10
+}
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
