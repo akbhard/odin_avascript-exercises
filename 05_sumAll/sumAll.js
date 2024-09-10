fd4bd9f2f08408dbdbdb25250067805b037c389c
Akshay <149981521+akbhard@users.noreply.github.com>
2024-09-09 23:57:28 -0400

    Exercise 5. Liam helped once again. Thank you Liam

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,29 @@
-const sumAll = function() {
+function sumAll(a,b) {
 
-};
-
+    if (a < 0 || b < 0){
+      return 'ERROR'
+    }
+    
+    let lowVariable, highVariable
+  
+    if (a < 0 || b < 0 || !Number.isInteger(a) || !Number.isInteger(b)) {
+      return 'ERROR'
+    } else if (a < b) {
+      lowVariable = a
+      highVariable = b
+    } else if (a > b) {
+      highVariable = a
+      lowVariable = b
+    }
+    
+  
+    let sum = lowVariable  
+    while (lowVariable < highVariable) {
+  
+      lowVariable++
+      sum += lowVariable
+    }
+    return sum
+  }
 // Do not edit below this line
 module.exports = sumAll;
