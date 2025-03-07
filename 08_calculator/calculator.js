a625deed59c38cdc042a3858cfc42f56fdb73166
Akshay <149981521+akbhard@users.noreply.github.com>
2025-03-06 21:30:31 -0500

    Exercise 8 for Odin  Javascript Exercises

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,27 +1,41 @@
-const add = function() {
+const add = function(a,b) {
+	return a + b
+};
+
+const subtract = function(a,b) {
+	return a - b
+};
+
+const sum = function(array) {
+  return array.reduce((total, currentItem) => {
+    return total + currentItem;
+  }, 0);
+};
+
+const multiply = function(array) {
+  return array.reduce((total,currentItem) => {
+    return total * currentItem;
+  },1)
+};
+
+// console.log(multiply([2,4,6,8,10,12,14]))
+
+const power = function(a,b) {
+  return Math.pow(a,b)
 	
 };
 
-const subtract = function() {
-	
+const factorial = function(num) {
+	if (num < 0) 
+        return -1;
+  else if (num == 0) 
+      return 1;
+  else {
+      return (num * factorial(num - 1));
+  }
 };
 
-const sum = function() {
-	
-};
-
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
-};
-
+console.log(factorial(10))
 // Do not edit below this line
 module.exports = {
   add,
