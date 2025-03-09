07df2d4ece62a94457697e6a88a77fdc72d52a43
Akshay <149981521+akbhard@users.noreply.github.com>
2025-03-09 14:30:31 -0400

    Fibonacci solution. Was pretty difficult, a lot of googling required. Need to use pseduo code more

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    num = parseInt(num);
 
-};
+    let [a,b] = [0,1];
+    if (num < 0) {
+        return "OOPS"
+    } else if (num === 0) {
+        return 0;
+    }
 
+    while (--num > 0) {
+        [a,b] = [b, a+b]
+    }
+    return b;
+}
+
+console.log(fibonacci("25"))
 // Do not edit below this line
 module.exports = fibonacci;
