a30e64aa8ee4957b37aae84855c0dd83bb2b60d5
Akshay <149981521+akbhard@users.noreply.github.com>
2025-03-09 13:22:50 -0400

    Palindrome checker complete. Cleaned strings first then ran reverse via split, reverse, and join

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,15 @@
-const palindromes = function () {
+const palindromes = function (palindrome) {
+    const cleaned = palindrome.toLowerCase().replace(/[\W_]/g, "")
+    const reverse = cleaned.split("").reverse().join("");
 
+    if (cleaned == reverse) {
+        return true
+    } else {
+        return false
+    }
 };
+
+console.log(palindromes('r3ace3car'))
 
 // Do not edit below this line
 module.exports = palindromes;
